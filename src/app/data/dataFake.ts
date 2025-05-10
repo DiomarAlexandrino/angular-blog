ce0567c1362ba4c50f13526331b24c1d6c33207e
Diomar Alexandrino <diomar.alexandrino@gmail.com>
2025-05-10 13:29:35 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,21 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Em um lugar escuro estamos nós. E mais conhecimento ilumina nosso caminho.",
+    'photoCover': "https://media.gazetadopovo.com.br/2013/02/07afb2140229e1c4bd5744b0a7646e48-full.jpg",
+    'description': "Lorem ipsum dolor sit amet"
+  },
+  {
+    'id': "2",
+    'title': "Notícia 2",
+    'photoCover': "https://i0.wp.com/dropsdejogos.uai.com.br/wp-content/uploads/sites/10/2025/03/star-wars-2-scaled.jpg?resize=950%2C532&ssl=1",
+    'description': "Lorem ipsum dolor sit amet"
+  },
+  {
+    'id': "3",
+    'title': "Notícia 3",
+    'photoCover': "https://i0.wp.com/dropsdejogos.uai.com.br/wp-content/uploads/sites/10/2025/03/star-wars-2-scaled.jpg?resize=950%2C532&ssl=1",
+    'description': "Lorem ipsum dolor sit amet"
+    },
+  
+  ]
