cf59e2e6b1b5b80c2211b1f43c2240925758d0ee
Diomar Alexandrino <diomar.alexandrino@gmail.com>
2025-05-11 22:07:58 -0300

    ajuste de layout

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,21 +1,21 @@
 export const dataFake = [
   {
     "id": "1",
-    "title": "Em um lugar escuro estamos nós. E mais conhecimento ilumina nosso caminho.",
+    "title": "Que a Força esteja com você!",
     'photoCover': "https://media.gazetadopovo.com.br/2013/02/07afb2140229e1c4bd5744b0a7646e48-full.jpg",
-    'description': "Lorem ipsum dolor sit amet"
+    'description': "Em um lugar escuro estamos nós. E mais conhecimento ilumina nosso caminho."
   },
   {
     'id': "2",
-    'title': "Notícia 2",
+    'title': "O medo é o caminho para o lado negro.",
     'photoCover': "https://i0.wp.com/dropsdejogos.uai.com.br/wp-content/uploads/sites/10/2025/03/star-wars-2-scaled.jpg?resize=950%2C532&ssl=1",
-    'description': "Lorem ipsum dolor sit amet"
+    'description': "Esteja consciente de seus pensamentos. Eles traem você"
   },
   {
     'id': "3",
-    'title': "Notícia 3",
+    'title': "Faça ou não faça. A tentativa não existe",
     'photoCover': "https://i0.wp.com/dropsdejogos.uai.com.br/wp-content/uploads/sites/10/2025/03/star-wars-2-scaled.jpg?resize=950%2C532&ssl=1",
-    'description': "Lorem ipsum dolor sit amet"
+    'description': "Treine a si mesmo a deixar partir tudo que teme perder"
     },
   
   ]
